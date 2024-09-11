a24346dae665a6631e412afa97cef6a53f1fa583
Winifred <bluestar3426@gmail.com>
2024-09-11 23:18:52 +0800

    Add dummy data and link to pages

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,61 @@
 from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+all_posts = [
+    {
+        "slug": "My-Dinner",
+        "image": "swords.png",
+        "author": "Winifred",
+        "date": date(2024, 9, 11),
+        "title": "My Dinner",
+        "excerpt": "Curry is the best!",
+        "content":"""
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem recusandae excepturi cumque deleniti odio quae. Sint sapiente, eaque velit explicabo nam libero fugit amet ullam repudiandae facere sed dolor perspiciatis.
+        """
+    },
+    {
+        "slug": "My-Work",
+        "image": "cat.jpg",
+        "author": "Winifred",
+        "date": date(2024, 9, 11),
+        "title": "My Work",
+        "excerpt": "Curry is the best!",
+        "content":"""
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem recusandae excepturi cumque deleniti odio quae. Sint sapiente, eaque velit explicabo nam libero fugit amet ullam repudiandae facere sed dolor perspiciatis.
+            
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem recusandae excepturi cumque deleniti odio quae. Sint sapiente, eaque velit explicabo nam libero fugit amet ullam repudiandae facere sed dolor perspiciatis.
+        """
+    },
+    {
+        "slug": "My-Friend",
+        "image": "house.jpg",
+        "author": "Winifred",
+        "date": date(2024, 9, 11),
+        "title": "My Friend",
+        "excerpt": "Curry is the best!",
+        "content":"""
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem recusandae excepturi cumque deleniti odio quae. Sint sapiente, eaque velit explicabo nam libero fugit amet ullam repudiandae facere sed dolor perspiciatis.
+        """
+    }
+]
+
+def get_date(post):
+    return post['date']
 
 def starting_page(request):
-    return render(request, "posts/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "posts/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
-    return render(request, "posts/all-posts.html")
+    return render(request, "posts/all-posts.html", {
+        "all_posts": all_posts
+    })
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "posts/post-detail.html", {
+        "post": identified_post
+    })
